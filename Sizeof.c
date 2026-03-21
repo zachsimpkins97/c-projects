9c4aa6f54bbb5b698f7f2dc0699b14373ec8b351
Zachariah Simpkins <zachariahsimpkins19@gmail.com>
2026-03-21 18:03:44 -0400

    Add sizeof demo program

---
+++ Sizeof.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Size of int : %lu\n", sizeof(int));
+
+    printf("Size of float : %lu\n", sizeof(float));
+
+    printf("Size of double :  %lu\n", sizeof(double));
+
+    printf("Size of char : %lu\n", sizeof(char));
+
+    return 0;
+}
